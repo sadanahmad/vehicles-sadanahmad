fc3e9839f9e39ce4c3fcf87057e3431bc350ca4d
SADAN AHMAD <37300997+sadanahmad@users.noreply.github.com>
2023-01-21 17:02:04 +0530

    Update WaterVehicle.java

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+//public interface WaterVehicle {
+//
+//}
+//
+//
+//package com.driver;
+
 public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
